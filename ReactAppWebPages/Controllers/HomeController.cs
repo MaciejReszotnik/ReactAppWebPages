854c3a80235e6c367de2a7765d5ceca967fff1ac
Maciej Reszotnik <Maciej.Reszotnik@ivanti.com>
2019-02-14 17:41:06 +0100

    Add project files.

---
+++ ReactAppWebPages/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactAppWebPages.Models;
+
+namespace ReactAppWebPages.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            var product = new Product {
+                ProductID = 1111,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M
+            };
+
+            return View(product);
+        }
+    }
+}
